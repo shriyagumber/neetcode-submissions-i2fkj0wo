a66b8071f1d7cddd8087b4137b66c1736f83c72e
Shriya Gumber <84539282+shriyagumber@users.noreply.github.com>
2026-04-03 10:48:22 -0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # valid tree means there should be no cycles
+        # using a set to see every node that is enountered, and if it is encountered again, then false
+
+        adj = [set() for _ in range(n)]
+
+        for u, v in edges:
+            adj[u].add(v)
+            adj[v].add(u)
+
+        self.count = 0
+        def dfs(curr: int, parent: int, visited: set) -> bool:
+            visited.add(curr)
+            self.count += 1
+
+            for child in adj[curr]:
+                if child == parent: continue
+                if child in visited or not dfs(child, curr, visited):
+                    return False
+            
+            return True
+
+        if not dfs(0, -1, set()) or self.count != n:
+            return False
+        return True
+        
+        
